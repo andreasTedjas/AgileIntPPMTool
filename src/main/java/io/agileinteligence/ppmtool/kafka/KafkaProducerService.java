cad1384aa8fd5a525b2c8017de4aec689ca5bc41
Andreas Sukmana <andreas.sukmana@btpn.com>
2020-01-27 17:36:34 +0700

    Revert "remove kafka"
    This reverts commit fbb5c424

---
+++ src/main/java/io/agileinteligence/ppmtool/kafka/KafkaProducerService.java
@@ -0,0 +1,36 @@
+package io.agileinteligence.ppmtool.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Service
+public class KafkaProducerService {
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    private static final String topicName = "topic1";
+
+    public void sendMessage(String message) {
+        ListenableFuture<SendResult<String, String>> future =
+                kafkaTemplate.send(topicName, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + message + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+}
